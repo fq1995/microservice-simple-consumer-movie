149eac799a6e046cfa18a8f609a3510379c8c149
付强 <1152237929@qq.com>
2017-12-26 22:17:00 +0800

    movie

---
+++ src/main/java/com/fu/cloud/controller/MoverController.java
@@ -0,0 +1,22 @@
+package com.fu.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.fu.cloud.entity.User;
+
+@RestController
+public class MoverController {
+	
+	@Autowired 
+	private RestTemplate restTemplate;
+	
+	@GetMapping("/movie/{id}")
+	public User findById(@PathVariable Long id){
+		return this.restTemplate.getForObject("http://localhost:8080/simple/"+ id, User.class);
+	}
+	
+}
